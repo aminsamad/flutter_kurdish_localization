982847a8da66e81fe990a4fa95feced2e0bddd2c
M Atheer <masreplay@gmail.com>
2022-05-18 09:55:41 +0300

    fix: :bug: can't import file

--- lib/kurdish_localization_delegates.dart
+++ lib/kurdish_localization_delegates.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'kurdish_cupertino_localization_delegate.dart';
 import 'kurdish_material_localization_delegate.dart';
 import 'kurdish_widget_localization_delegate.dart';
 
-const List<LocalizationsDelegate<dynamic>> kurdishLocalizationsDelegates =
-    <LocalizationsDelegate<dynamic>>[
-  KurdishMaterialLocalizations.delegate,
-  KurdishWidgetLocalizations.delegate,
-  KurdishCupertinoLocalizations.delegate,
-];
+class KurdishLocalizations {
+  static const kurdishLocalizationsDelegates = <LocalizationsDelegate<dynamic>>[
+    KurdishMaterialLocalizations.delegate,
+    KurdishWidgetLocalizations.delegate,
+    KurdishCupertinoLocalizations.delegate,
+  ];
+  static const LocalizationsDelegate<MaterialLocalizations>
+      kurdishMaterialLocalizations = KurdishMaterialLocalizations.delegate;
+
+  static const LocalizationsDelegate<WidgetsLocalizations>
+      kurdishWidgetLocalizations = KurdishWidgetLocalizations.delegate;
+
+  static const LocalizationsDelegate<CupertinoLocalizations>
+      kurdishCupertinoLocalizations = KurdishCupertinoLocalizations.delegate;
+}
